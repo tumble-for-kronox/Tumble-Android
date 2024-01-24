88759b02f5ead0d38f4a6675bfbb1d14c0d05006
adisve <adis.veletanlic@gmail.com>
2024-01-24 14:30:15 +0100

    Fix bottom bar nav

--- app/src/main/java/tumble/app/tumble/presentation/views/account/Account.kt
+++ app/src/main/java/tumble/app/tumble/presentation/views/account/Account.kt
@@ -1,8 +1,9 @@
 package tumble.app.tumble.presentation.views.account
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Account() {
-    
+    Text("Showing account")
 }
--- app/src/main/java/tumble/app/tumble/presentation/views/bookmarks/Bookmarks.kt
+++ app/src/main/java/tumble/app/tumble/presentation/views/bookmarks/Bookmarks.kt
@@ -1,8 +1,9 @@
 package tumble.app.tumble.presentation.views.bookmarks
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Bookmarks() {
-
+    Text("Showing bookmarks")
 }
