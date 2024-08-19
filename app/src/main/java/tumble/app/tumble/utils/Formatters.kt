978188d90c41c7f21154dd8e3f802280ef252217
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-08-19 16:11:56 +0200

    add formater to get full month name from date

--- app/src/main/java/tumble/app/tumble/utils/Formatters.kt
+++ app/src/main/java/tumble/app/tumble/utils/Formatters.kt
@@ -4,12 +4,17 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun Date.toIsoString(): String {
-    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.getDefault())
+    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
     return format.format(this)
 }
 
 val isoDateFormatter: SimpleDateFormat by lazy {
     val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
     formatter.timeZone = TimeZone.getDefault()
+    formatter
+}
+
+val month_date: SimpleDateFormat by lazy {
+    val formatter = SimpleDateFormat("MMMM", Locale.getDefault())
     formatter
 }
