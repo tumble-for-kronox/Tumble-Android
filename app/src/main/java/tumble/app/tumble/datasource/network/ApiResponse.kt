1b598c94ecda631d4a88c6bbefe4742bdace329d
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-08-30 09:11:27 +0200

    removed olf files

--- app/src/main/java/tumble/app/tumble/datasource/network/ApiResponse.kt
+++
@@ -1,9 +0,0 @@
-package tumble.app.tumble.datasource.network
-
-import tumble.app.tumble.domain.models.network.NetworkRequest
-
-sealed class ApiResponse<T> {
-    class Success<T>(val data: T) : ApiResponse<T>()
-    class Error<T>(val errorMessage: String, val errorCode: Int? = null) : ApiResponse<T>()
-    class Loading<T> : ApiResponse<T>()
-}
