7f368b3e9f4d928a677a55b7218277c3c40ae633
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-08-30 10:12:47 +0200

    model and extension for resources

---
+++ app/src/main/java/tumble/app/tumble/extensions/models/NetworkResponseAvailabilitiesExtension.kt
@@ -0,0 +1,30 @@
+package tumble.app.tumble.extensions.models
+
+import tumble.app.tumble.domain.models.network.NetworkResponse
+import tumble.app.tumble.domain.models.network.availabilities
+
+fun availabilities.timelotHasAvailable(timelotId: Int):Boolean {
+
+    val availabilities = this ?: return false
+    for ((_, availabilityValues) in availabilities){
+        val availability = availabilityValues[timelotId]?.availability
+
+        if (availability == NetworkResponse.AvailabilityEnum.AVAILABLE){
+            return true
+        }
+    }
+    return false
+}
+
+fun availabilities.getAvailabilityValues(timelotId: Int): List<NetworkResponse.AvailabilityValue>{
+
+    val availabilities = this?: return emptyList()
+    val availabilityValueResult = mutableListOf<NetworkResponse.AvailabilityValue>()
+    for ((_, availabilityValues) in availabilities){
+        val availabilityValue = availabilityValues[timelotId]
+        if(availabilityValue?.availability == NetworkResponse.AvailabilityEnum.AVAILABLE){
+            availabilityValueResult.add(availabilityValue)
+        }
+    }
+    return availabilityValueResult
+}
