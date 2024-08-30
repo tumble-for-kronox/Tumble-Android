f4c2a9e39be5161743167ae1da02b9649a7d81f3
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-08-30 10:08:20 +0200

    app appearence changes with settings(there is probably a better way of doing this)

--- app/src/main/java/tumble/app/tumble/presentation/viewmodels/ParentViewModel.kt
+++ app/src/main/java/tumble/app/tumble/presentation/viewmodels/ParentViewModel.kt
@@ -5,14 +5,19 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import tumble.app.tumble.datasource.network.kronox.KronoxRepository
-import tumble.app.tumble.datasource.preferences.DataStoreManager
-import tumble.app.tumble.datasource.realm.RealmManager
+import tumble.app.tumble.data.repository.preferences.DataStoreManager
+import tumble.app.tumble.data.repository.realm.RealmManager
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
+import tumble.app.tumble.data.repository.preferences.CombinedData
+import tumble.app.tumble.domain.enums.Types.AppearanceType
+import tumble.app.tumble.domain.enums.ViewType
 
 data class CombinedData(val authSchoolId: Int, val userOnBoarded: Boolean)
+
 
 @HiltViewModel
 class ParentViewModel @Inject constructor(
@@ -21,19 +26,28 @@ class ParentViewModel @Inject constructor(
     private val dataStoreManager: DataStoreManager
 ): ViewModel() {
 
-    private val _combinedData = MutableStateFlow(CombinedData(-1, false))
+    private val _combinedData = MutableStateFlow(CombinedData(-1, false, ViewType.LIST, false))
     val combinedData: StateFlow<CombinedData> = _combinedData
+
+    private val _appearance = MutableStateFlow<AppearanceType>(AppearanceType.AUTOMATIC)
+    val appearance: StateFlow<AppearanceType> = _appearance
 
     init {
         observeDataStoreChanges()
     }
 
+
     private fun observeDataStoreChanges() {
         viewModelScope.launch {
             dataStoreManager.authSchoolId.combine(dataStoreManager.userOnBoarded) { authSchoolId, userOnBoarded ->
-                CombinedData(authSchoolId, userOnBoarded)
+                CombinedData(authSchoolId = authSchoolId, userOnBoarded = userOnBoarded)
             }.collect { combinedData ->
                 _combinedData.value = combinedData
+            }
+        }
+        viewModelScope.launch {
+            dataStoreManager.appearance.collect{
+                _appearance.value = it
             }
         }
     }
--- app/src/main/java/tumble/app/tumble/presentation/views/AppParent.kt
+++ app/src/main/java/tumble/app/tumble/presentation/views/AppParent.kt
@@ -1,12 +1,18 @@
 package tumble.app.tumble.presentation.views
 
 import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
@@ -17,11 +23,16 @@ import tumble.app.tumble.presentation.navigation.navgraphs.SearchNavGraph
 import tumble.app.tumble.presentation.viewmodels.ParentViewModel
 import tumble.app.tumble.presentation.views.navigation.BottomBar
 import tumble.app.tumble.presentation.views.navigation.BottomNavItem
+import tumble.app.tumble.ui.theme.TumbleTheme
 
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Preview(showBackground = true, name = "AppParent preview")
 @Composable
 fun AppParent() {
+    val viewModel: ParentViewModel = viewModel()
+
+    val apperance  = viewModel.appearance.collectAsState()
 
     val accountNavController = rememberNavController()
     val bookmarksNavController = rememberNavController()
@@ -30,23 +41,31 @@ fun AppParent() {
 
     val currentNavGraph = remember { mutableStateOf(BottomNavItem.BOOKMARKS) }
 
-    val viewModel: ParentViewModel = viewModel()
     val combinedData by viewModel.combinedData.collectAsState()
 
-    Scaffold(
-        bottomBar = { BottomBar(
-            homeNavController,
-            bookmarksNavController,
-            searchNavController,
-            accountNavController,
-            currentNavGraph,
-        ) }
-    ) {
-        when (currentNavGraph.value) {
-            BottomNavItem.HOME -> HomeNavGraph(homeNavController)
-            BottomNavItem.BOOKMARKS -> BookmarksNavGraph(bookmarksNavController)
-            BottomNavItem.SEARCH -> SearchNavGraph(searchNavController)
-            BottomNavItem.ACCOUNT -> AccountNavGraph(accountNavController)
+    TumbleTheme(userPreferences =  apperance.value) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Scaffold(
+                bottomBar = {
+                    BottomBar(
+                        homeNavController,
+                        bookmarksNavController,
+                        searchNavController,
+                        accountNavController,
+                        currentNavGraph,
+                    )
+                }
+            ) {
+                when (currentNavGraph.value) {
+                    BottomNavItem.HOME -> HomeNavGraph(homeNavController)
+                    BottomNavItem.BOOKMARKS -> BookmarksNavGraph(bookmarksNavController)
+                    BottomNavItem.SEARCH -> SearchNavGraph(searchNavController)
+                    BottomNavItem.ACCOUNT -> AccountNavGraph(accountNavController)
+                }
+            }
         }
     }
 }
