da9c6500d99919d8ef12adb8544202e30e09dacb
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-08-30 09:43:42 +0200

    elements from notification branch

---
+++ app/src/main/java/tumble/app/tumble/domain/models/TumbleUser.kt
@@ -0,0 +1,6 @@
+package tumble.app.tumble.domain.models
+
+data class TumbleUser(
+    var username: String,
+    var name: String
+)
---
+++ app/src/main/java/tumble/app/tumble/domain/models/presentation/ViewType.kt
@@ -0,0 +1,7 @@
+package tumble.app.tumble.domain.models.presentation
+
+enum class ViewType(val displayName: String) {
+    LIST("List"),
+    CALENDAR("Calendar"),
+    WEEK("Week")
+}
