e54b76c0b59390ac440c8789192f6cb40c5934c5
adisve <adis.veletanlic@gmail.com>
2024-10-05 20:47:49 +0200

    Fix dev env switch

--- app/src/main/java/tumble/app/tumble/core/NetworkSettings.kt
+++ app/src/main/java/tumble/app/tumble/core/NetworkSettings.kt
@@ -5,17 +5,15 @@ import tumble.app.tumble.BuildConfig
 class NetworkSettings private constructor(val port: Int, val scheme: String, val tumbleUrl: String) {
     companion object {
         val shared: NetworkSettings = when (BuildConfig.BUILD_TYPE) {
-            "debug" -> Environments.production
+            "debug" -> Environments.development
             else -> Environments.production
         }
 
         object Environments {
-            // Production URL
             val production = NetworkSettings(
                 port = 443, scheme = "https", tumbleUrl = "app.tumbleforkronox.com"
             )
 
-            // Debug URL
             val development = NetworkSettings(
                 port = 80, scheme = "https", tumbleUrl = "localhost"
             )
