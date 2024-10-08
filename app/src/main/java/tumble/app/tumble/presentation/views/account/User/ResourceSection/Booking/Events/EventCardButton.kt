cb5ebed8fec055909c84bb07a8d3cfb356cee009
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-10-08 09:33:46 +0200

    refactor events

--- app/src/main/java/tumble/app/tumble/presentation/views/account/User/ResourceSection/Booking/Events/EventCardButton.kt
+++ app/src/main/java/tumble/app/tumble/presentation/views/account/User/ResourceSection/Booking/Events/EventCardButton.kt
@@ -2,9 +2,16 @@ package tumble.app.tumble.presentation.views.account.User.ResourceSection.Bookin
 
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
@@ -20,18 +27,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import tumble.app.tumble.R
 import tumble.app.tumble.domain.models.network.NetworkResponse
 import tumble.app.tumble.extensions.presentation.convertToHoursAndMinutesISOString
+import tumble.app.tumble.extensions.presentation.formatDate
 import tumble.app.tumble.extensions.presentation.isValidRegistrationDate
+import tumble.app.tumble.extensions.presentation.toColor
 import tumble.app.tumble.extensions.presentation.toLocalDateTime
 import tumble.app.tumble.utils.isoDateFormatterDate
 
-enum class EventType(val icon: String) {
-    REGISTER("check_circle"),
-    UNREGISTER("cancel")
+enum class EventType {
+    REGISTER,
+    UNREGISTER
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -41,95 +52,99 @@ fun EventCardButton(
     eventType: EventType,
     onTap: () -> Unit
 ) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 10.dp)
-            .background(MaterialTheme.colors.surface, shape = RoundedCornerShape(15.dp)) // Replace with your theme colors
-            .padding()
+            .background(MaterialTheme.colors.surface, shape = RoundedCornerShape(20.dp))
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.weight(1f)
         ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier.weight(1f)
-            ) {
-                // Event Title
-                Text(
-                    text = event.title ?: "stringResource(id = R.string.no_title)",
-                    fontSize = 17.sp,
-                    color = MaterialTheme.colors.onSurface, // Replace with your theme colors
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
+            // Event Title
+            Text(
+                text = event.title ?: "stringResource(id = R.string.no_title)",
+                fontSize = 17.sp,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            // Event Date and Time
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = rememberVectorPainter(Icons.Default.CalendarToday),
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
                 )
-                // Event Date and Time
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(
-                        painter = rememberVectorPainter(Icons.Default.CalendarToday),
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.onSurface.copy(alpha = 0.7f) // Replace with your theme colors
-                    )
-                    val eventDate = event.eventStart.toLocalDateTime()
-                    val eventStart = event.eventStart.convertToHoursAndMinutesISOString()
-                    val eventEnd = event.eventEnd.convertToHoursAndMinutesISOString()
-                    val eventDateText = if (eventDate != null && eventStart != null && eventEnd != null) {
-                        String.format("stringResource(id = R.string.event_time_format)", eventDate, eventStart, eventEnd)
+                val eventDate = isoDateFormatterDate.format(isoDateFormatterDate.parse(event.eventStart))
+                val eventStart = event.eventStart.convertToHoursAndMinutesISOString()
+                val eventEnd = event.eventEnd.convertToHoursAndMinutesISOString()
+                val eventDateText =
+                    if (eventDate != null && eventStart != null && eventEnd != null) {
+                        "$eventDate: $eventStart - $eventEnd"
                     } else {
                         "stringResource(id = R.string.no_date_at_this_time)"
                     }
-                    Text(
-                        text = eventDateText,
-                        fontSize = 15.sp,
-                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f) // Replace with your theme colors
-                    )
-                }
-                // Signup Date
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(
-                        painter = rememberVectorPainter(Icons.Default.Edit),
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.onSurface.copy(alpha = 0.7f) // Replace with your theme colors
-                    )
-                    val signupText = if (event.lastSignupDate.isValidRegistrationDate()) {
-                        "${"stringResource(id = R.string.available_until)"} ${isoDateFormatterDate.parse(event.lastSignupDate) ?: "stringResource(id = R.string.no_date_set)"}"
-                    } else {
-                        "stringResource(id = R.string.signup_has_passed)"
-                    }
-                    Text(
-                        text = signupText,
-                        fontSize = 15.sp,
-                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f) // Replace with your theme colors
-                    )
-                }
+                Text(
+                    text = eventDateText,
+                    fontSize = 15.sp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
+                )
             }
-            Spacer(modifier = Modifier.width(10.dp))
-        }
-        if (event.lastSignupDate.isValidRegistrationDate()) {
-            Row(
-                horizontalArrangement = Arrangement.End,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Button(
-                    onClick = { event.eventId?.let { onTap() } },
-                    modifier = Modifier
-                        .background(MaterialTheme.colors.primary, shape = RoundedCornerShape(10.dp)) // Replace with your theme colors
-                        .padding(10.dp)
+            // Signup Date
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = rememberVectorPainter(Icons.Default.Edit),
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
+                )
+                val signupText = if (event.lastSignupDate.isValidRegistrationDate()) {
+                    "${stringResource(id = R.string.available_until)} ${
+                        isoDateFormatterDate.format(
+                            isoDateFormatterDate.parse(event.lastSignupDate)
+                        ) ?: "stringResource(id = R.string.no_date_set)"
+                    }"
+                } else {
+                    stringResource(id = R.string.signup_has_passed)
+                }
+                Text(
+                    text = signupText,
+                    fontSize = 15.sp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
+                )
+            }
+            if (event.lastSignupDate.isValidRegistrationDate()) {
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    Icon(
-                        painter = rememberVectorPainter(Icons.Default.Event),
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.onPrimary// Replace with your theme colors
-                    )
-                    Spacer(modifier = Modifier.width(5.dp))
-                    Text(
-                        text = if (eventType == EventType.UNREGISTER) "stringResource(id = R.string.unregister)" else "stringResource(id = R.string.register)",
-                        fontSize = 16.sp,
-                        color = MaterialTheme.colors.onPrimary// Replace with your theme colors
-                    )
+                    Button(
+                        onClick = {
+                            event.eventId?.let { onTap() } },
+                        modifier = Modifier
+                            .background(
+                                MaterialTheme.colors.primary,
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .padding(10.dp)
+                    ) {
+                        Icon(
+                            painter = rememberVectorPainter(Icons.Default.Event),
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onPrimary
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = if (eventType == EventType.UNREGISTER) stringResource(id = R.string.unregister) else stringResource(id = R.string.register),
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colors.onPrimary
+                        )
+                    }
                 }
             }
         }
+        Spacer(modifier = Modifier.width(10.dp))
     }
 }
--- app/src/main/java/tumble/app/tumble/presentation/views/account/User/ResourceSection/Booking/Events/Events.kt
+++ app/src/main/java/tumble/app/tumble/presentation/views/account/User/ResourceSection/Booking/Events/Events.kt
@@ -2,112 +2,102 @@ package tumble.app.tumble.presentation.views.account.User.ResourceSection.Bookin
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import tumble.app.tumble.domain.models.network.NetworkResponse
+import tumble.app.tumble.R
 import tumble.app.tumble.domain.models.network.NetworkResponse.AvailableKronoxUserEvent
 import tumble.app.tumble.domain.models.network.NetworkResponse.UpcomingKronoxUserEvent
 
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun Events(
-    registeredEvents: List<AvailableKronoxUserEvent>? = null,
-    unregisteredEvents: List<AvailableKronoxUserEvent>? = null,
-    upcomingEvents: List<UpcomingKronoxUserEvent>? = null,
-    onTapEventAction: ((String, EventType) -> Unit)? = null
-) {
-    unregisteredEventsView(unregisteredEvents, onTapEventAction)
-    registeredEventsView(registeredEvents, onTapEventAction)
-    upcomingEventsView(upcomingEvents)
-}
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun registeredEventsView(
-    registeredEvents: List<NetworkResponse.AvailableKronoxUserEvent>?,
-    onTapEventAction: ((String, EventType) -> Unit)?
+fun RegisteredEventsView(
+    registeredEvents: List<AvailableKronoxUserEvent>,
+    onTapEventAction: ((String, EventType) -> Unit)
 ) {
     Column {
-        registeredEvents?.let { events ->
-            onTapEventAction?.let { action ->
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(15.dp),
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    events.forEach { event ->
-                        EventCardButton(
-                            event = event,
-                            eventType = EventType.UNREGISTER,
-                            onTap = { action(event.eventId!!, EventType.UNREGISTER) }
-                        )
-                    }
+        if (registeredEvents.isNotEmpty()) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(15.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                registeredEvents.forEach { event ->
+                    EventCardButton(
+                        event = event,
+                        eventType = EventType.UNREGISTER,
+                        onTap = { onTapEventAction(event.eventId!!, EventType.UNREGISTER) }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
                 }
             }
-            if (events.isEmpty()) {
-                Text(
-                    text = "stringResource(id = R.string.no_registered_events)",
-                    modifier = Modifier
-                        .padding(top = 5.dp)
-                )
-            }
+        }else {
+            Text(
+                text = stringResource(id = R .string.no_registered_events),
+                modifier = Modifier
+                    .padding(top = 5.dp)
+            )
         }
     }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun unregisteredEventsView(
-    unregisteredEvents: List<NetworkResponse.AvailableKronoxUserEvent>?,
-    onTapEventAction: ((String, EventType) -> Unit)?
+fun UnregisteredEventsView(
+    unregisteredEvents: List<AvailableKronoxUserEvent>,
+    onTapEventAction: ((String, EventType) -> Unit)
 ) {
     Column {
-        unregisteredEvents?.let { events ->
-            onTapEventAction?.let { action ->
-                Column(modifier = Modifier.fillMaxWidth()) {
-                    events.forEach { event ->
-                        EventCardButton(
-                            event = event,
-                            eventType = EventType.REGISTER,
-                            onTap = { action(event.eventId!!, EventType.REGISTER) }
-                        )
-                    }
-                }
-            }
-            if (events.isEmpty()) {
-                Text(
-                    text = "stringResource(id = R.string.no_unregistered_events)",
-                    modifier = Modifier
-                        .padding(top = 5.dp)
-                )
-            }
-        }
-    }
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun upcomingEventsView(
-    upcomingEvents: List<NetworkResponse.UpcomingKronoxUserEvent>?
-) {
-    Column {
-        upcomingEvents?.let { events ->
+        if (unregisteredEvents.isNotEmpty()) {
             Column(modifier = Modifier.fillMaxWidth()) {
-                events.forEach { event ->
+                unregisteredEvents.forEach { event ->
+                    EventCardButton(
+                        event = event,
+                        eventType = EventType.REGISTER,
+                        onTap = { onTapEventAction(event.eventId!!, EventType.REGISTER) }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        }
+        else{
+            Text(
+                text = stringResource(id = R.string.no_unregistered_events),
+                modifier = Modifier
+                    .padding(top = 5.dp)
+            )
+        }
+    }
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun UpcomingEventsView(
+    upcomingEvents: List<UpcomingKronoxUserEvent>
+) {
+    Column {
+        if (upcomingEvents.isNotEmpty()) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                upcomingEvents.forEach { event ->
                     UpcomingEventCardButton(event = event)
                     Spacer(modifier = Modifier.height(8.dp))
                 }
             }
-            if (events.isEmpty()) {
-                Text(
-                    text = "stringResource(id = R.string.no_upcoming_events)",
-                    modifier = Modifier
-                        .padding(top = 5.dp)
+        } else{
+            Text(
+                text = stringResource(id = R.string.no_upcoming_events),
+                modifier = Modifier
+                    .padding(top = 5.dp)
                 )
             }
         }
     }
-}
